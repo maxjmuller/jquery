4fadb4ac7143360d05995c0c084396e67a3f7aa9
Jeff Seymour & Max Muller <student@epicodus.com>
2015-09-30 19:46:40 -0700

    add clickable walrus image

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
 jQuery(document).ready(function() {
 
-  jQuery("h1").click(function() {
-    alert("This is a header.");
-  });
+  // jQuery("h1").click(function() {
+  //   alert("This is a header.");
+  // });
+  //
+  // jQuery("ul").click(function() {
+  //   alert("This is a unordered list");
+  // });
+  //
+  // jQuery("img").hover(function() {
+  //   alert("This is a paragraph");
+  // });
+  //
+  // jQuery(".intro").dblclick(function() {
+  //   alert("This is a paragraph");
+  // });
+  //
+  // jQuery("p").click(function() {
+  //   alert("This is a paragraph");
+  // });
 
-  jQuery("ul").click(function() {
-    alert("This is a unordered list");
-  });
+  $(".clickable").click(function() {
+    $(".initially-hidden").toggle();
+    $(".initially-showing").toggle();
+  })
 
-  jQuery("img").hover(function() {
-    alert("This is a paragraph");
-  });
-
-  jQuery(".intro").dblclick(function() {
-  alert("This is a paragraph");
-  });
-
-  jQuery("p").click(function() {
-    alert("This is a paragraph");
-  });
 
 
 
