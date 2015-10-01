ea847ab6816bbdc7c5c33874921e7be9d7100305
Jeff Seymour & Max Muller <student@epicodus.com>
2015-09-30 19:08:52 -0700

    Add addition jquery functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,23 @@ jQuery(document).ready(function() {
   jQuery("h1").click(function() {
     alert("This is a header.");
   });
-  
+
+  jQuery("ul").click(function() {
+    alert("This is a unordered list");
+  });
+
+  jQuery("img").hover(function() {
+    alert("This is a paragraph");
+  });
+
+  jQuery(".intro").dblclick(function() {
+  alert("This is a paragraph");
+  });
+
+  jQuery("p").click(function() {
+    alert("This is a paragraph");
+  });
+
+
+
 });
